db5750223c58ed5ba18386639e3e71905bd7e341
gn-grc <genejogarcia.gg@gmail.com>
2021-07-20 17:18:19 +0800

    added env gitignore

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,6 @@
 
-const DB_USERNAME = '';
-const DB_PASSWORD = '';
+// const DB_USERNAME = ''; in environment variable
+// const DB_PASSWORD = '';
 const DB_ATLAS_URI = '';
 
 const DB_NAME = 'sikreto';
@@ -8,6 +8,6 @@ const LOCAL_URI = `mongodb://localhost:27017/${DB_NAME}`;
 
 exports.dbURI = () => {
 
-    if (process.env) return LOCAL_URI;
+    if (process.env.DB_PASSWORD || process.env.DB_USERNAME) return LOCAL_URI;
     else return DB_ATLAS_URI;
 }

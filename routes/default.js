3a6f7f35e8e33cabf439e4dd3733da060dde2ca1
gn-grc <genejogarcia.gg@gmail.com>
2021-07-20 16:12:57 +0800

    implemented database upload from register page

--- routes/default.js
+++ routes/default.js
@@ -1,18 +1,42 @@
 // package
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+// db models
+const Account = mongoose.model('Account');
 
 // routes
 router.get('/register', (req, res) => {
     res.render('register');
-})
+});
 
 router.post('/register',(req, res) => {
     console.log(req.body);
 
-    res.end();
-})
+    // research for the best to handle validation modularly
+    // check for null values
 
+    // password validation
+
+    // database validation
+
+    // check if email and username exists
+
+    const newAccount = Account({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+    newAccount.save((err) => {
+        if (err){
+            console.log(err);
+            res.redirect('/register');
+        } else {
+            res.send('success');
+        }
+    })
+});
 
 // export router for app.js
 module.exports = router;

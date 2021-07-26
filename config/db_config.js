dce8458d701299e6027149b65ef07d147c9ff0f2
gn-grc <genejogarcia.gg@gmail.com>
2021-07-26 11:17:36 +0800

    uploaded database to mongodb atlas

--- config/db_config.js
+++ config/db_config.js
@@ -1,12 +1,13 @@
-// require('dotenv').config();
-
-// const DB_USERNAME = ''; in environment variable
-// const DB_PASSWORD = '';
-const DB_ATLAS_URI = '';
+require('dotenv').config();
 
 const DB_NAME = 'sikreto';
+const DB_USERNAME = process.env.DB_USERNAME; 
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const DB_ATLAS_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@personal-cluster.sofzm.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
 const LOCAL_URI = `mongodb://localhost:27017/${DB_NAME}`;
 
 exports.dbURI = () => {
-    return LOCAL_URI;
+    return DB_ATLAS_URI;
 }
